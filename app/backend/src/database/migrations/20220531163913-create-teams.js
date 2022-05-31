221b67600a2dc16efe4e1318e913048fa2b4a764
Danilo Couto <couto.danilo95@gmail.com>
2022-05-31 17:12:28 -0300

    feat teams migration

---
+++ app/backend/src/database/migrations/20220531163913-create-teams.js
@@ -0,0 +1,24 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('teams', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      teamName: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        field: 'team_name',
+      },
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('teams');
+  }
+};
+
