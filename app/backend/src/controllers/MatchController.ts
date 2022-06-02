0d657ccfea6e3325c90b57bd9f19530cb599b50d
Danilo Couto <couto.danilo95@gmail.com>
2022-06-02 20:52:57 -0300

    feat matchController

--- app/backend/src/controllers/MatchController.ts
+++ app/backend/src/controllers/MatchController.ts
@@ -1,14 +1,13 @@
-/* import { Request, Response } from 'express';
+import { Request, Response } from 'express';
+import MatchService from '../services/MatchService';
 
 export default class MatchController {
-  constructor(private teamsService: ITeamService) { }
+  constructor(private matchService = new MatchService()) { }
 
   public findAll = async (req: Request, res: Response) => {
-    const teams = await this.teamsService.findAll();
-    return (!teams)
-      ? res.status(401).json({ message: 'Teams not found' })
-      : res.status(200).json(teams);
+    const matches = await this.matchService.findAll();
+    return (!matches)
+      ? res.status(401).json({ message: 'Matches not found' })
+      : res.status(200).json(matches);
   };
 }
-
- */
