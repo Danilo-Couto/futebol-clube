253438bfa5cbab535c44f99627941d771ccbd2bf
Danilo Couto <couto.danilo95@gmail.com>
2022-06-03 08:58:39 -0300

    feat createMatch

--- app/backend/src/controllers/MatchController.ts
+++ app/backend/src/controllers/MatchController.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from 'express';
+import { verifiedToken } from '../utils/Token';
 import MatchService from '../services/MatchService';
 
 export default class MatchController {
@@ -7,7 +8,20 @@ export default class MatchController {
   public findAll = async (req: Request, res: Response) => {
     const matches = await this.matchService.findAll();
     return (!matches)
-      ? res.status(401).json({ message: 'Matches not found' })
+      ? res.status(401).json({ message: 'Erro: Matches not found' })
       : res.status(200).json(matches);
+  };
+
+  public create = async (req: Request, res: Response) => {
+    const token = req.headers.authorization;
+    if (!token) return res.status(401).json({ message: 'Token not found' });
+
+    const decoded = verifiedToken(token);
+    if (!decoded) return res.status(400).json({ message: 'Not Authorized' });
+
+    const match = await this.matchService.create(req.body);
+    return !match
+      ? res.status(401).json({ message: 'Erro: Match not created' })
+      : res.status(201).json(match);
   };
 }
